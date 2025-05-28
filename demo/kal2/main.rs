7db9e18a335d6ffff661ddcc4857ccd1c7d85a26
Vincent Jorgensen <vincent.jorgensen@gmail.com>
2025-05-27 20:53:16 -0700

    date selection and "now"

--- demo/kal2/main.rs
+++ demo/kal2/main.rs
@@ -1,16 +1,41 @@
+use chrono::{DateTime, Datelike, Local};
 use clap::Parser;
 use kalendarium::Kalendarium;
 
 #[derive(Parser)]
+#[command(version)]
 struct Cli {
-    year: String,
-    month: String,
-    day: String,
+    #[arg(default_value_t = -43)]
+    year: isize,
+    #[arg(default_value_t = 3)]
+    month: isize,
+    #[arg(default_value_t = 15)]
+    day: isize,
+
+    #[arg(short, long)]
+    now: bool,
 }
 
 fn main() {
     let args = Cli::parse();
 
-    let kal: Kalendarium = Kalendarium::new(&args.year, &args.month, &args.day).unwrap();
+    let kal: Kalendarium;
+
+    if args.now {
+        let date_time: DateTime<Local> = chrono::offset::Local::now();
+        kal = Kalendarium::new(
+            &date_time.year().to_string(),
+            &date_time.month().to_string(),
+            &date_time.day().to_string(),
+        )
+        .unwrap();
+    } else {
+        kal = Kalendarium::new(
+            &args.year.to_string(),
+            &args.month.to_string(),
+            &args.day.to_string(),
+        )
+        .unwrap();
+    }
     println!("{}", kal);
 }
